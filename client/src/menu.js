8894f140f21c38bc0965b8e6e7e7bb930439f6b8
marcelo hernan <marcelo.fernandezc@usm.cl>
2023-07-06 00:22:54 -0400

    boton menu y solo nombre

--- client/src/menu.js
+++ client/src/menu.js
@@ -8,33 +8,41 @@ const NavBar = ({ user }) => {
 
   const logout = () => {
     window.location.href = "https://www.google.com";
-  }
+  };
 
   const myFinances = () => {
     window.location.href = "https://www.google.com";
-  }
+  };
 
   const handleMenuClick = () => {
     setIsOpen(!isOpen);
-  }
+  };
 
   return (
     <div className="navbar">
+      <div className="navbar__menuContainer">
+        <button className="navbar__menuButton" onClick={handleMenuClick}>
+          Menú
+        </button>
+        {isOpen && (
+          <div className="navbar__dropdown">
+            <button className="navbar__button" onClick={logout}>
+              Cerrar sesión
+            </button>
+            <button className="navbar__button" onClick={myFinances}>
+              Mis finanzas
+            </button>
+          </div>
+        )}
+      </div>
       <div className="navbar__content">
-        <h1 className="heading">Menú</h1>
-        <button className="navbar__menuButton" onClick={handleMenuClick}>Menú</button>
-      {isOpen && (
-        <div className="navbar__dropdown">
-          <button className="navbar__button" onClick={logout}>Cerrar sesión</button>
-          <button className="navbar__button" onClick={myFinances}>Mis finanzas</button>
-        </div>
-      )}
         <h2 className="navbar__name">Nombre: {user.name}</h2>
-        <p className="navbar__email">Correo: {user.email}</p>
       </div>
     </div>
   );
 };
+
+
 
 const Menu = () => {
   const { id } = useParams();
